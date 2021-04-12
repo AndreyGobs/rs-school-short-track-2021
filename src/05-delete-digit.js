6861ffdb2359c3fba779ae38a27ac2abbaab4b17
AndreyGobs <55401672+AndreyGobs@users.noreply.github.com>
2021-04-12 19:24:30 +0300

    feat: add delete digit func

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,19 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const str = String(n).split('');
+  const arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const number = str.concat();
+    number.splice(i, 1);
+    arr.push(+number.join(''));
+  }
+
+  arr.sort((a, b) => b - a);
+
+  return arr[0];
 }
 
 module.exports = deleteDigit;
