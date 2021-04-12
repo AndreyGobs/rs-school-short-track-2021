33bdc5a6423f522dc723c96b13f6a6b634a46fef
AndreyGobs <55401672+AndreyGobs@users.noreply.github.com>
2021-04-12 19:18:44 +0300

    feat: add func matrix element

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,22 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix = [[]]) {
+  const rejectedColumns = new Set();
+  let result = 0;
+  matrix.forEach((row) => {
+    for (let j = 0; j < row.length; j++) {
+      const item = row[j];
+      if (item === 0) {
+        rejectedColumns.add(j);
+      }
+      if (!rejectedColumns.has(j)) {
+        result += item;
+      }
+    }
+  });
+  return result;
+  // throw new Error('Not implemented');
 }
 
 module.exports = getMatrixElementsSum;
