9d3abc39fa0311713497d128945cac0baeff4a74
AndreyGobs <55401672+AndreyGobs@users.noreply.github.com>
2021-04-12 19:32:49 +0300

    feat: add encode line func

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = [];
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === str.charAt(i + 1)) {
+      count++;
+    } else if (str.charAt(i) !== str.charAt(i + 1) && count > 1) {
+      result.push(`${count}${str.charAt(i)}`);
+      count = 1;
+    } else if (str.charAt(i) !== str.charAt(i + 1) && count === 1) {
+      result.push(`${str.charAt(i)}`);
+      count = 1;
+    }
+  }
+
+  result = result.join('');
+
+  return result;
 }
 
 module.exports = encodeLine;
